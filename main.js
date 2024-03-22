d9ff36ac9e12773d0f94fdb9158f2cee0ac61484
Sajjadahmedshaikh <160344749+Sajjadahmedshaikh@users.noreply.github.com>
2024-03-21 18:18:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+// let food="biryani"//error dega q k pehlay he  declair kar chuke hain
+// console.log(food)
+// var food="biryani";//error dega q k pehlay he  declair kar chuke hain
+// var food= "haleem";//error dega q k pehlay he  declair kar chuke hain
+// console.log (food)
+// const food="haleem"// error dega q k pehlay he  declair kar chuke hain..
+// console.log(food)
+var num1 = 1;
+var num2 = "1";
+console.log(num1 + num2);
---
+++ main.ts
@@ -0,0 +1,20 @@
+
+console.log("hello world");
+
+// let food="biryani"//error dega q k pehlay he  declair kar chuke hain
+// console.log(food)
+
+// var food="biryani";//error dega q k pehlay he  declair kar chuke hain
+
+// var food= "haleem";//error dega q k pehlay he  declair kar chuke hain
+// console.log (food)
+
+// const food="haleem"// error dega q k pehlay he  declair kar chuke hain..
+// console.log(food)
+ 
+
+let num1=1
+ let num2="1"
+console.log(num1+num2)
+
+
